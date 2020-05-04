9955fe0a71a5282ec38434db1046b70a5cb7e49a
IlyaGrshin <grishinilja@gmail.com>
2020-05-04 14:15:20 +0300

    dev build fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path')
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
-
-  // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
@@ -15,18 +13,12 @@ module.exports = (env, argv) => ({
 
   module: {
     rules: [
-      // Converts TypeScript code to JavaScript
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
       { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
-
-      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
       { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] },
     ],
   },
 
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
   resolve: { 
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
     "alias": {
@@ -37,10 +29,9 @@ module.exports = (env, argv) => ({
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    path: path.resolve(__dirname, 'dist'),
   },
 
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/ui.html',
